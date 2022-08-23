3d5a0c6273df13c80453669fc1ee250286e7fd02
Luisa Dala <111546558+Fwessa@users.noreply.github.com>
2022-08-23 11:27:16 +0800

    Create App.java

---
+++ Constructor/App.java
@@ -0,0 +1,59 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Creates a new object named "Knight" from Character class
+        Character knight = new Character(10, 5, 0, "Chen");
+        // Calls the sayMyName method
+        // Calls the sayMyStrength method
+        // Calls the sayMyAgility method
+        // Calls the sayMyIntelligence method
+        knight.sayMyName();
+        knight.sayMyStrength();
+        knight.sayMyAgility();
+        knight.sayMyIntelligence();
+
+        // Creates a new object named "mage" from Character class
+        Character mage = new Character(0, 5, 10, "Akasha");
+        // Calls the sayMyName method
+        // Calls the sayMyStrength method
+        // Calls the sayMyAgility method
+        // Calls the sayMyIntelligence method
+        mage.sayMyName();
+        mage.sayMyStrength();
+        mage.sayMyAgility();
+        mage.sayMyIntelligence();
+
+        // Creates a new object named "thief" from Character class
+        Character thief = new Character(5, 10, 0, "Gondar");
+        // Calls the sayMyName method
+        // Calls the sayMyStrength method
+        // Calls the sayMyAgility method
+        // Calls the sayMyIntelligence method
+        thief.sayMyName();
+        thief.sayMyStrength();
+        thief.sayMyAgility();
+        thief.sayMyIntelligence();
+
+        // Creates a new object named "healer" from Character class
+        Character healer = new Character(30, 100, 90, "Fwessa");
+        // Calls the sayMyName method
+        // Calls the sayMyStrength method
+        // Calls the sayMyAgility method
+        // Calls the sayMyIntelligence method
+        healer.sayMyName();
+        healer.sayMyStrength();
+        healer.sayMyAgility();
+        healer.sayMyIntelligence();
+
+        // Creates a new object named "tank" from Character class
+        Character tank = new Character(100, 20, 40, "Kento");
+        // Calls the sayMyName method
+        // Calls the sayMyStrength method
+        // Calls the sayMyAgility method
+        // Calls the sayMyIntelligence method
+        tank.sayMyName();
+        tank.sayMyStrength();
+        tank.sayMyAgility();
+        tank.sayMyIntelligence();
+    }
+}
